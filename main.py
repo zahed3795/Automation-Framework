7540eda3db97ed82c91d0661ad8bcacf9299718c
zahed <47490281+zahed3795@users.noreply.github.com>
2020-10-28 09:34:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,12 @@ class MyPlugin:
     pass
 
 
-pytest.main(["-s", '-v','-n 2', '--junitxml=Report/report.xml', '--html=Reports/report.html'], plugins=[MyPlugin()])
+pytest.main(["-s", '-v','-n 2', '--junitxml=Reports/report.xml', '--html=Reports/report.html'], plugins=[MyPlugin()])
+
+Report = os.path.dirname(os.path.realpath(__file__))
+HTML_Report = Report + "/pytest_html_report.html"
+if os.path.exists(HTML_Report):
+    os.remove(HTML_Report)
+JSON_Report = Report + "/output.json"
+if os.path.exists(JSON_Report):
+    os.remove(JSON_Report)
