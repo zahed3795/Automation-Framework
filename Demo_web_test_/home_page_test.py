7ed6cb049fcd0fbb18d72e40c1d798e20a5cc98e
zahed <47490281+zahed3795@users.noreply.github.com>
2020-11-01 12:11:46 -0500

    Update home_page_test.py

--- Demo_web_test_/home_page_test.py
+++ Demo_web_test_/home_page_test.py
@@ -1,5 +1,6 @@
 import time
 
+from masterQA.core.string import String
 from masterQA.fixtures.base_case import BaseCase
 from masterQA.data.settings import environment as env
 from masterQA.common import encryption
@@ -9,9 +10,9 @@ USER_EMAIL = encryption.decrypt("$^*ENCRYPT=RkVdG1VpaWYoRCc3MksZNQ91e1tCRg==?&#$
 USER_PASSWORD = encryption.decrypt("$^*ENCRYPT=ZWIjSx5uEhVYGwUQ?&#$")
 
 
-class MyTestClass(BaseCase):
+class MyTestClass(BaseCase, String):
 
-    def test_anything(self):
+    def test_open_browser(self):
         # Write your code here. Example:
         self.Open(env.URL)
         self.Maximize_wendow()
@@ -25,7 +26,7 @@ class MyTestClass(BaseCase):
         time.sleep(1)
         self.tearDown()
 
-    def test_anything2(self):
+    def test_Scroll_Into_View(self):
         # Write your code here. Example:
         self.Open(env.URL)
         self.Click("//a[contains(text(),'News')]")
@@ -35,9 +36,14 @@ class MyTestClass(BaseCase):
         self.Click("//a[contains(text(),'Crypto')]")
         self.tearDown()
 
-    def test_01(self):
+    def test_save_file(self):
         self.Open(env.URL)
         url_link = self.get_unique_links()
         self.save_data_as(url_link, 'myfile.txt')
 
-
+    def test_string_funtions(self):
+        # Write your code here. Example:
+        self.convert_to_lower_case('<----ABCD---->')  # Convert string into lower case
+        self.convert_to_upper_case('<----abcd---->')  # Convert string into upper case
+        self.get_line_count('\n1..a,\n2..a,\n3..a,\n4..a')  # Get line count from string
+        self.split_to_lines("\nfirst \n2nd \n3rd", 2)  # Split lines from string as parameter need argument
