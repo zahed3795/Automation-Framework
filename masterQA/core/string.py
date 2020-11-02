4160c0570c8e5fc1404b85d218e8e3296f524116
zahed <47490281+zahed3795@users.noreply.github.com>
2020-11-02 15:30:07 -0500

    Update string.py

--- masterQA/core/string.py
+++ masterQA/core/string.py
@@ -28,22 +28,22 @@ def _convert_to_integer(value, name):
 class StringBase:
 
     def convert_to_lower_case(self, string):
-        return print(lower("\n" + str(string)))
+        return lower("\n" + str(string))
 
     def convert_to_upper_case(self, string):
-        return print(upper("\n" + str(string)))
+        return upper("\n" + str(string))
 
     def get_line_count(self, string):
         """Returns and logs the number of lines in the given string."""
         count = len(string.splitlines())
-        return print(count)
+        return count
 
     def split_to_lines(self, string, start=0, end=None):
         """Splits the given string to lines"""
         start = _convert_to_index(start, 'start')
         end = _convert_to_index(end, 'end')
         lines = string.splitlines()[start:end]
-        return print(lines)
+        return lines
 
     def get_line(self, string, line_number):
         """Returns the specified line from the given ``string``."""
