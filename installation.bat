8b381a28e1e2e04e63404af23f16e840abc3ce83
zahed <47490281+zahed3795@users.noreply.github.com>
2020-11-03 01:11:19 -0500

    Update installation.bat

--- installation.bat
+++ installation.bat
@@ -2,11 +2,66 @@
 python --version
 pip install -r requirements.txt
 python setup.py install
-
-
-
-
-
-
-
-
+webdriver_manager==3.2.2
+pip install packaging>=20.4
+pip install setuptools>=50.3.2
+pip install setuptools-scm
+pip install wheel>=0.35.1
+pip install six
+pip install nose
+pip install ipdb
+pip install parso>=0.7.1
+pip install jedi>=0.17.2
+pip install idna>=2.10
+pip install chardet>=3.0.4
+pip install urllib3>=1.25.11
+pip install requests>=2.24.0
+pip install selenium>=3.141.0
+pip install msedge-selenium-tools>=3.141.2
+pip install more-itertools>=8.6.0
+pip install cssselect>=1.1.0
+pip install pluggy>=0.13.1
+pip install attrs>=20.2.0
+pip install py>=1.9.0
+pip install pytest>=6.1.2
+pip install pytest-cov>=2.10.1
+pip install pytest-forked>=1.3.0
+pip install pytest-metadata>=1.10.0
+pip install pytest-ordering>=0.6
+pip install pytest-rerunfailures>=9.1.1
+pip install pytest-xdist>=2.1.0
+pip install parameterized>=0.7.4
+pip install soupsieve>=2.0.1
+pip install beautifulsoup4>=4.9.3
+pip install cryptography>=3.2.1
+pip install pyopenssl>=19.1.0
+pip install pygments>=2.7.2
+pip install traitlets>=5.0.5
+pip install prompt-toolkit>=3.0.8
+pip install ipython>=7.19.0
+pip install colorama>=0.4.4
+pip install pip install importlib-metadata>=2.0.0
+pip install virtualenv>=20.1.0
+pip install pymysql>=0.10.1
+pip install coverage>=5.3
+pip install brython>=3.9.0
+pip install pyotp>=2.4.1
+pip install boto>=2.49.0
+pip install cffi>=1.14.3
+pip install pip install rich>=9.1.0
+pip install zipp>=3.4.0
+pip install flake8>=3.8.4
+pip install pyflakes>=2.2.0
+pip install tornado>=6.1
+pip install certifi>=2020.6.20
+pip install allure-pytest>=2.8.19
+pip install pdfminer.six>=20201018
+pip install pytest-json-report>=1.2.1
+pip install pytest-json>=0.4.0
+pip install pytest-html-reporter>=0.2.3
+pip install pytest-bdd>=4.0.1
+pip install pytest-html>=2.1.1
+pip install pytest-bdd>=4.0.1
+pip install pytest-bdd-web>=0.1.1
+pip install pytest-metadata>=1.10.0
+pip install pytest-ordering>=0.6
